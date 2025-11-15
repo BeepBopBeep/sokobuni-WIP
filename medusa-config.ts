e025e9717ee59b93689d8d3b5a41cd11bd296516
Gideon Ngaruiya <gideon@geeks.co.ke>
2025-11-15 14:29:20 +0300

    Ready to Deploy

--- medusa-config.ts
+++ medusa-config.ts
@@ -12,7 +12,13 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
+  redisUrl: process.env.REDIS_URL,
+
+  },
+  admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
   },
   modules: [
     {
